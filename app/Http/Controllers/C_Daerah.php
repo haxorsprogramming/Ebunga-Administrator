e1260896fc390336a328cf373dd6b6dcf7b3df2e
haxorsprogramming <haxorsuinsu@gmail.com>
2021-01-27 16:48:00 +0700

    update

---
+++ app/Http/Controllers/C_Daerah.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C_Daerah extends Controller
+{
+    //
+}
---
+++ app/Models/M_Order_Details.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class M_Order_Details extends Model
+{
+    use HasFactory;
+}
