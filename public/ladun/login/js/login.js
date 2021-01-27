8d7ef18dc824c86d624a03f612d18af7d1fa64e3
haxorsprogramming <haxorsuinsu@gmail.com>
2021-01-27 16:31:17 +0700

    update

--- public/ladun/login/js/login.js
+++ public/ladun/login/js/login.js
@@ -1,3 +1,17 @@
+// vue object
+var divLogin = new Vue({
+    el : '#divLogin',
+    data : {
+
+    },
+    methods : {
+
+    }
+});
+
+// inisialisasi
+document.querySelector("#txtUsername").focus();
+
 $("#pass").click(function() {
     $("#show").attr("type", "text");
     $(this).hide();
